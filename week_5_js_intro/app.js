4511917a83498dd688b5870648c09c775555875c
tommychipman <tpchipman@gmail.com>
2015-09-27 21:36:35 -0400

    Homework 1,2,3
    I got the first one but couldn’t figure out the while loop. I’m stuck
    on the function for a larger number.

---
+++ week_5_js_intro/app.js
@@ -0,0 +1,36 @@
+
+
+// for(var i =0; i < 21; i++) {
+// 	if (i%2 === 0) {
+// 		console.log(i + " is even");
+// 	} else {
+// 		console.log(i + " is odd");
+// 	}
+// }
+
+// var  x = 0 < 21;
+
+// while( x < 21 ){
+// 	x++;
+// 	console.log(x + " is even");
+// 	console.log(x = "is odd");
+// }
+
+// var num1 = 10;
+// var num2 = 100;
+
+// function max(){
+// 	if (num2 > num1) 
+//     answer = true;
+// }
+// console.log(max());
+
+function max(num1,num2){	
+	if(num2 > num1){
+		alert(num2 + " is the greater number")
+		return a;
+	}else{
+		alert( num2 +" is the greater number");
+		return b;
+	}
+	}
